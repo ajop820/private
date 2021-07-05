dc96a3868926ee96335daf6338027b0563ca1587
ajop820 <vemreais@gmail.com>
2021-07-05 12:59:02 -0300

    First commit

---
+++ PRIVATE-master/src/alugar.js
@@ -0,0 +1,18 @@
+const alugar = () => { 
+	return `           
+╔══✪〘 ALUGAR 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 30 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO,PIX, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/554384028569*
+║
+╚═〘  DEXP 〙
+`
+}
+exports.alugar = alugar
---
+++ PRIVATE-master/src/metodos.js
@@ -0,0 +1,14 @@
+const metodos = (prefix, sender) => {
+	return `DEXP METODOS VIP
+
+*${prefix}mt1* Painel Kiny
+*${prefix}mt2* Em breve
+*${prefix}mt3* Em breve
+*${prefix}mt4* Em breve
+*${prefix}mt5* Em breve
+
+METODOS AGREGADOS POR DEXP BOT
+` 
+}
+
+exports.metodos = metodos
